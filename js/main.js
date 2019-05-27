c6de2f462ed7219e03091e03fbc7fd1c0a3e25ca
HongyiLiou <t30034a7@gmail.com>
2019-05-27 11:02:41 +0800

    update

--- js/main.js
+++ js/main.js
@@ -13,64 +13,9 @@ navBtns.forEach(function(navBtn){
 
 
 
-// Header 背景 (捲動相關 Fade In) ______________________________________。<>>
-const header = document.querySelector('.header')
-const topBtn = document.querySelector('#topBtn')
+// 物件進入視窗範圍 Fade In ____________________________________________。<>>
 const photo = document.querySelector('.photo')
 const works = document.querySelectorAll('.works')
-
-window.addEventListener('scroll', function(){
-    let photoPosTop = photo.getBoundingClientRect().top
-    let photoPosBottom = photo.getBoundingClientRect().bottom
-
-    if(this.scrollY > 0) {
-        header.classList.add('active')
-        topBtn.classList.add('active')
-    }
-    else {
-        header.classList.remove('active')
-        topBtn.classList.remove('active')
-    }
-
-    // fadeIn
-    if(photoPosTop < window.innerHeight && photoPosBottom > 200) {
-        photo.classList.add('fadeIn')
-    }
-
-    works.forEach(function(work){
-        let workPosTop = work.getBoundingClientRect().top
-        let workPosBottom = work.getBoundingClientRect().bottom
-        if(workPosTop < window.innerHeight && workPosBottom > 200) {
-            work.classList.add('fadeIn')
-        }
-    })
-})
-
-
-
-// topBtn 回到頂端按鈕 ________________________________________________。<>>
-$('#topBtn').click(function(){  //回到頂端按鈕
-    $('html,body').animate({
-        scrollTop: 0
-    },500)
-})
-
-
-
-// Nav 導航按鈕 _______________________________________________________。<>>
-$('#intro_nav').click(function(){
-    $('html,body').animate({scrollTop:$('#intro').offset().top - 100},500)
-})
-$('#works_nav').click(function(){
-    $('html,body').animate({scrollTop:$('#allworks').offset().top},500)
-})
-$('#contact_nav').click(function(){
-    $('html,body').animate({scrollTop:$('#contact').offset().top},500)
-})
-
-
-
-// 物件進入視窗範圍 Fade In ____________________________________________。<>>
 
 function fadeIn(){
     let photoPosTop = photo.getBoundingClientRect().top
@@ -89,4 +34,57 @@ function fadeIn(){
     })
 }
 
-window.onload = fadeIn
+
+
+// Header 背景 (捲動相關 Fade In) ______________________________________。<>>
+const header = document.querySelector('.header')
+const topBtn = document.querySelector('#topBtn')
+
+window.addEventListener('scroll', function(){
+    if(this.scrollY > 0) {
+        header.classList.add('active')
+        topBtn.classList.add('active')
+    }
+    else {
+        header.classList.remove('active')
+        topBtn.classList.remove('active')
+    }
+
+    fadeIn()
+})
+
+
+
+// topBtn 回到頂端按鈕 ________________________________________________。<>>
+$('#topBtn').click(function(){  //回到頂端按鈕
+    $('html,body').animate({
+        scrollTop: 0
+    },500)
+})
+
+function topBtnReload(){
+    if(window.scrollY > 0) {
+        header.classList.add('active')
+        topBtn.classList.add('active')
+    }
+}
+
+
+
+// Nav 導航按鈕 _______________________________________________________。<>>
+$('#intro_nav').click(function(){
+    $('html,body').animate({scrollTop:$('#intro').offset().top - 100},500)
+})
+$('#works_nav').click(function(){
+    $('html,body').animate({scrollTop:$('#allworks').offset().top},500)
+})
+$('#contact_nav').click(function(){
+    $('html,body').animate({scrollTop:$('#contact').offset().top},500)
+})
+
+
+
+window.onload = function(){
+    fadeIn()
+    topBtnReload()
+}
